511a56c9cc177b2fe61b663c642701ee627d6202
vmt07816 <58446101+vmt07816@users.noreply.github.com>
2020-01-14 23:04:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
-const fs = require('fs')
-const youtubedl = require('youtube-dl')
+const fs = require('fs');
+const youtubedl = require('youtube-dl');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -41,14 +41,14 @@ app.get('/downloadmp4', (req,res) => {
 	  // Optional arguments passed to youtube-dl.
 	  ['--format=18'],
 	  // Additional options can be given for calling `child_process.execFile()`.
-	  { cwd: __dirname })
+	  { cwd: __dirname });
 	 
 	// Will be called when the download starts.
 	video.on('info', function(info) {
 	  console.log('Download started')
 	  console.log('filename: ' + info._filename)
 	  console.log('size: ' + info.size)
-	})
+	});
 	 
 	video.pipe(fs.createWriteStream('myvideo.mp4'))
 });
