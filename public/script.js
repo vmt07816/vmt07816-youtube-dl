fb848559798b415d6b2b56fb67ef73ddd2236673
vmt07816 <58446101+vmt07816@users.noreply.github.com>
2020-01-14 23:36:26 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var mp3Btn = document.getElementById('mp3');
 var	mp4Btn = document.getElementById('mp4');
+var	imgBtn = document.getElementById('image');
 var URLinput = document.querySelector('.URL-input');
 // var server = '';
 
@@ -13,11 +14,17 @@ mp4Btn.addEventListener('click', () => {
 	console.log(`URL: ${URLinput.value}`);	
 	redirectMp4(URLinput.value);
 });
-
+imgBtn.addEventListener('click', () => {
+	console.log(`URL: ${URLinput.value}`);	
+	redirectImage(URLinput.value);
+});
 function redirectMp3(query) {
 	window.location.href = `downloadmp3?url=${query}`;
 }
 
 function redirectMp4(query) {
 	window.location.href = `downloadmp4?url=${query}`;
+}
+function redirectImage(query) {
+	window.location.href = `downloadimg?url=${query}`;
 }
